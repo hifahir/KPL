1310b0c940143eb9a17560e9acfe088faa7021b0
hifahir <putratertuahirzi@gmail.com>
2023-03-05 19:05:49 +0700

    commit pertama

---
+++ TP_MOD3_1302213092/Form1.cs
@@ -0,0 +1,15 @@
+namespace TP_MOD3_1302213092
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
