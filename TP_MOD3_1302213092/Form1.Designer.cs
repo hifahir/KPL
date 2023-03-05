285ff233c734167c665d3b08c9cd5c53171d6350
hifahir <putratertuahirzi@gmail.com>
2023-03-05 19:51:02 +0700

    commit GUI

--- TP_MOD3_1302213092/Form1.Designer.cs
+++ TP_MOD3_1302213092/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TP_MOD3_1302213092
 {
-    partial class Form1
+    partial class Welcome
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,19 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
+            btnClickThis = new Button();
+            label1 = new Label();
+            txtNama = new TextBox();
             SuspendLayout();
             // 
-            // Form1
+            // btnClickThis
+            // 
+            btnClickThis.Location = new Point(195, 55);
+            btnClickThis.Name = "btnClickThis";
+            btnClickThis.Size = new Size(94, 29);
+            btnClickThis.TabIndex = 0;
+            btnClickThis.Text = "Click This";
+            btnClickThis.UseVisualStyleBackColor = true;
+            btnClickThis.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(112, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(121, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Masukkan Nama:";
+            label1.Click += label1_Click;
+            // 
+            // txtNama
+            // 
+            txtNama.Location = new Point(251, 22);
+            txtNama.Name = "txtNama";
+            txtNama.Size = new Size(177, 27);
+            txtNama.TabIndex = 2;
+            // 
+            // Welcome
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Name = "Form1";
+            ClientSize = new Size(512, 110);
+            Controls.Add(txtNama);
+            Controls.Add(label1);
+            Controls.Add(btnClickThis);
+            Name = "Welcome";
             Text = "Form1";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnClickThis;
+        private Label label1;
+        private TextBox txtNama;
     }
 }
--- TP_MOD3_1302213092/Form1.cs
+++ TP_MOD3_1302213092/Form1.cs
@@ -1,13 +1,23 @@
 namespace TP_MOD3_1302213092
 {
-    public partial class Form1 : Form
+    public partial class Welcome : Form
     {
-        public Form1()
+        public Welcome()
         {
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Halo " + txtNama.Text + "!");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
